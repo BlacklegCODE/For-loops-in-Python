337b72b8c1dc49ac9f0e59fcc05498262b1c9ab1
Harsh Raundal <dkingston0000@gmail.com>
2024-11-16 11:09:36 +0530

    Add files via upload

---
+++ For loop practice.py
@@ -0,0 +1,30 @@
+#FOR LOOP PRACTICE
+'''
+
+
+for i in reversed (range(1, 21, 4)):
+    print(i)
+print("Happy Birthday !")
+
+
+for x in range(1,50):
+    if x == 20:
+        continue
+    else:
+        print(x)
+
+
+
+for i in range(2,51,2):
+    if i == 24:
+        continue
+    else:
+        print(i)
+
+for i in range(2,51,2):
+    if i == 24:
+        break
+    else:
+        print(i)
+
+'''
